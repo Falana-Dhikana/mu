475dc7cc06b4ff4ab3cba0aadef9e75d2b237f11
Aloneboy <71880160+aloneboy121@users.noreply.github.com>
2024-05-31 11:31:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("AUTH_USERS")
+    BOT_TOKEN = os.environ.get("7101988842:AAGB9pFCCLP5HN8eokbVwXYBwZSa_GSuBGs")
+    API_ID = int(os.environ.get("26395620"))
+    API_HASH = os.environ.get("906cc7e4b4c1ad7f7121efc2244d13ad")
+    AUTH_USERS = os.environ.get("7047183902")
