c35d9df2b9ccfd9b8d2c8751df9df7ed5832a736
Falana <125483872+Falana-Dhikana@users.noreply.github.com>
2024-06-27 21:50:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("AUTH_USERS")
+    BOT_TOKEN = "6130954817:AAHh_nedYXEH7bhEaMRTsaZSAFucCvVe-wU"
+    API_ID = int(14852801)
+    API_HASH = "d4cee9b844c63eb5b1bcc8e3426beb40"
+    AUTH_USERS = "1832139347"
